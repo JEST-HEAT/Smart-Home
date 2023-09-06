254ff4adab59996c5644610b99f435cdea43b2f8
dansarpong <dansarpong.me@gmail.com>
2023-09-06 13:34:12 +0000

    Stop work

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
 
 app.config['MQTT_BROKER_URL'] = 'mqtt.dioty.co'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_USERNAME'] = 'smart-home'
 app.config['MQTT_PASSWORD'] = ''
 app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+app.config['MQTT_KEEPALIVE'] = 5
+
 mqtt = Mqtt(app)
 
 @app.route('/')
